0a13616e3007582133332affb4154db30d1fe5f8
Annica Alienus <anbj1001@student.miun.se>
2018-10-31 13:58:30 +0100

    lagt till id på Program

--- SSTV/Models/StaticResourses/Lists/StaticLists.cs
+++ SSTV/Models/StaticResourses/Lists/StaticLists.cs
@@ -30,79 +30,79 @@ namespace SSTV.Models.StaticResources.Lists
             channel = new TVChannel { ID = 1, Name = "SVT1" };
             channel.TVPrograms = new List<Program>
             {
-                new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                new Program{ID= 1,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
 
-                 new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                 new Program{ID= 2,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
 
-                  new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                  new Program{ID= 3,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
 
-                   new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                   new Program{ID= 4,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
             };
 
             channel = new TVChannel { ID = 2, Name = "SVT2" };
             channel.TVPrograms = new List<Program>
             {
-              new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+              new Program{ID= 5,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
 
-                 new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                 new Program{ID= 6,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
 
-                  new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                  new Program{ID= 7,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
 
-                   new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                   new Program{ID= 8,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
             };
 
             channel = new TVChannel { ID = 3, Name = "TV3" };
             channel.TVPrograms = new List<Program>
             {
-                new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                new Program{ID= 9,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
 
-                 new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                 new Program{ID= 10,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
 
-                  new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                  new Program{ID= 11,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
 
-                   new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                   new Program{ID= 12,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
             };
 
             channel = new TVChannel { ID = 4, Name = "TV4" };
             channel.TVPrograms = new List<Program>
             {
-               new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+               new Program{ID= 13,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
 
-                 new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                 new Program{ID= 14,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
 
-                  new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                  new Program{ID= 15,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
 
-                   new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                   new Program{ID= 16,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
             };
             channel = new TVChannel { ID = 5, Name = "Kanal 5" };
             channel.TVPrograms = new List<Program>
             {
-                new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                new Program{ID= 17,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
 
-                 new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                 new Program{ID= 18,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
 
-                  new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                  new Program{ID= 19,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
 
-                   new Program{Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
+                   new Program{ID= 20,Title = "",  Broadcasting = new DateTime(2018, 11, 05, 07, 00, 00),  Duration = 30,Informations = "This is some nice informations",
                     Categories = new List<Category>{ new Category { ID = 1, Name = "Thiller" }, new Category { ID = 3, Name = "Drama" } } },
             };
 
@@ -111,7 +111,7 @@ namespace SSTV.Models.StaticResources.Lists
         private void AddCategory()
         {
             Category category = new Category();
-
+            
             List<Category> categories = new List<Category>
             {
                 new Category { ID = 1, Name = "Thiller" },
